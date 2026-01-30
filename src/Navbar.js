4020ec4a49cdc408aaf8525138b5a22791aff236
d.toropova <d.toropova@g.nsu.ru>
2026-01-30 22:50:05 +0700

    added logo

--- src/Navbar.js
+++ src/Navbar.js
@@ -2,8 +2,12 @@ import { NavButton } from "./Buttons"
 
 const NavBar = () => {
     return ( 
-        <nav className="navigate-container">
+        <nav className="nav-container">
             <div className='nav-left'>
+                <div className="nav-logo">
+                    <img src="/Icons/mainIcon.svg"/>
+                    <h1>ABSOLUTE<br />CINEMA</h1>
+                </div>
                 <NavButton imageSrc="./Icons/profileIcon.svg" text="Профиль" />
                 <NavButton imageSrc="./Icons/ticketIcon.svg" text="Билеты" />
             </div>
